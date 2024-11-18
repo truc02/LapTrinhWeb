b0a0eb393c160dcdf2aa18c22bd170d1049cdbb1
khoaanhbui24 <buianhkhoa23022004@gmail.com>
2024-11-18 14:00:42 +0700

    khoa2

--- Frontend/header_successfull.php
+++ Frontend/header_successfull.php
@@ -72,9 +72,10 @@ $result = $conn->query($sql);
         display: none;
         position: absolute;
         background-color: #f9f9f9;
-        min-width: 160px;
-        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-        z-index: 1;
+        min-width: 200px;
+        box-shadow: -8px 8px 16px 0px rgba(0,0,0,0.2);
+        z-index: 10;
+        left: -50px;
     }
 
     .dropdown-content a {
